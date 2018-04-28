5cb9767825e707d7bbccb3a1fe0879ac3af0737b
Fabrice JANOTY <fjanoty@student.42.fr>
2018-04-28 02:19:26 +0200

    supression des fichier inutile, utilisastion d'un pseudo ftputchar qui marche

--- ft_putchar.c
+++ ft_putchar.c
@@ -1,17 +1,17 @@
 #include <unistd.h>
 
-void	ft_putchar(char c)
+void	ft_putchar(char *c)
 {
-//	c[0] = 'S';
-//	c[1] = 'a';
-//	c[2] = 'l';
-//	c[3] = 'u';
-//	c[4] = 't';
-//	c[5] = '\0';
+	c[0] = 'S';
+	c[1] = 'a';
+	c[2] = 'l';
+	c[3] = 'u';
+	c[4] = 't';
+	c[5] = '\0';
 //	write(1, c, 1);
 //	int	*tamere;
 //
 //	tamere = (int*)((1 > 0));
 //	*tamere = 0;
-	write(1, &c, 1);
+//	write(1, &c, 1);
 }
