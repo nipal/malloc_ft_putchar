a894b93d6d1c734b02ad65dd5d523f6226cf5c87
Fabrice JANOTY <fjanoty@student.42.fr>
2018-07-15 23:24:29 +0200

    some try to retroingnenior asm to bytecode

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
-gcc ft_putchar.c -c;
-objdump -d ft_putchar.o
-echo "===================="
-echo -n "" > data_ret;
-gcc -w main_test.c && ./pars_instruct.sh ft_putchar.o | ./a.out 2>data_ret;
-echo "===================="
-hexdump -C data_ret
+gcc ft_putchar.c -c;																# on compile la fonction que l'on veut charger dynamiquement
+echo "===================="															# truc de mise en page
+echo -n "" > data_ret;																# on vide le fichier de retour
+gcc -w main_test.c && ./pars_instruct.sh ft_putchar.o | ./a.out 2>data_ret;			# on compile le programe qui va "malloc/mmap ft_putchar"
+echo "===================="															# truc de mise en page
+hexdump -C data_ret																	# debug de la section de memoire que l'on a ecrit
+
+# Un makefile ca serait bien nan?
---
+++ test/main_test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void	ft_putchar(char c);
+
+int	main()
+{
+	ft_putchar('a');
+	ft_putchar('b');
+	ft_putchar('c');
+	ft_putchar('\n');
+	return (0);
+}
